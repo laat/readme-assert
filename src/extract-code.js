c75604f1ed09e9c885e7ca5180cb579f7e59c49c
Sigurd Fosseng <sigurd@fosseng.net>
2016-04-15 18:33:54 +0200

    remove remark

--- src/extract-code.js
+++ src/extract-code.js
@@ -1,48 +1,8 @@
-var remark = require('remark')
+import codeBlocks from 'gfm-code-blocks'
 
 export default function extractCode (markdown) {
-  let tests = []
-  remark = remark.use(blockExtractor, {tests})
-  var AST = remark.parse(markdown)
-  remark.run(AST) // transform
-  return tests
-}
-
-function blockExtractor (rmk, options = {tests: []}) {
-  return function (root) {
-    walk(root, visitor(options.tests))
-  }
-}
-
-function walk (tree, visitor) {
-  let children = tree.children || []
-  children.forEach((node) => {
-    visitor(node)
-    walk(node, visitor)
-  })
-}
-
-function visitor (tests) {
-  return (node) => {
-    if (node.type === 'code') {
-      let {lang} = parseLang(node.lang)
-      if (lang === 'javascript') {
-        // Silly github does not support tags
-        /* && tags.indexOf('test') >= 0 */
-        if (node.value.indexOf('//=>') >= 0 || node.value.indexOf('/*=>') >= 0) {
-          tests.push(node.value)
-        }
-      }
-    }
-  }
-}
-
-function parseLang (lang) {
-  if (!lang) {
-    return {}
-  }
-  return {
-    lang: lang.split(' ')[0],
-    tags: lang.split(' ').slice(1).filter((n) => !!n)
-  }
+  return codeBlocks(markdown)
+    .filter(({ lang }) => lang === 'javascript' || lang === 'js')
+    .filter(({ code }) => code.indexOf('//=>') >= 0 || code.indexOf('/*=>') >= 0)
+    .map(({code}) => code)
 }
