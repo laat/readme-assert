bfd9173176d1f7ba0a9aeaa2174bf9a68336e3cd
Sigurd Fosseng <sigurd@fosseng.net>
2019-06-08 01:01:01 +0200

    reorder

--- src/index.js
+++ src/index.js
@@ -9,37 +9,6 @@ import fs from "fs";
 import path from "path";
 import extract from "./extract";
 
-function prefixCode(code) {
-  const assertPath = require.resolve("assert-simple-tap");
-  return `var assert = require('${assertPath}');\n${code}`;
-}
-
-function evalCode(code, req = []) {
-  const args = ["-e", code, ...req.reduce((x, y) => x.concat("-r", y), [])];
-  const { status } = spawnSync("node", args, { stdio: "inherit" });
-  process.exit(status);
-}
-
-function read(file) {
-  return fs.readFileSync(path.join(process.cwd(), file), "utf-8");
-}
-
-function exists(file) {
-  try {
-    fs.statSync(path.join(process.cwd(), file));
-    return file;
-  } catch (err) {
-    return undefined;
-  }
-}
-
-function printCode(code) {
-  /* eslint-disable no-console */
-  console.log("# Testcode:");
-  code.split("\n").forEach((l, i) => console.log(`# ${i + 1} ${l}`));
-  /* eslint-enable no-console */
-}
-
 export default function run(main, req, shouldPrintCode, babelrc) {
   console.log({ babelrc });
   const pkg = JSON.parse(read("package.json"));
@@ -71,4 +40,33 @@ export default function run(main, req, shouldPrintCode, babelrc) {
   const prefixedCode = prefixCode(transformed);
   if (shouldPrintCode) printCode(prefixedCode);
   evalCode(prefixedCode, req);
+}
+
+function prefixCode(code) {
+  const assertPath = require.resolve("assert-simple-tap");
+  return `var assert = require('${assertPath}');\n${code}`;
+}
+
+function evalCode(code, req = []) {
+  const args = ["-e", code, ...req.reduce((x, y) => x.concat("-r", y), [])];
+  const { status } = spawnSync("node", args, { stdio: "inherit" });
+  process.exit(status);
+}
+
+function read(file) {
+  return fs.readFileSync(path.join(process.cwd(), file), "utf-8");
+}
+
+function exists(file) {
+  try {
+    fs.statSync(path.join(process.cwd(), file));
+    return file;
+  } catch (err) {
+    return undefined;
+  }
+}
+
+function printCode(code) {
+  console.log("# Testcode:");
+  code.split("\n").forEach((l, i) => console.log(`# ${i + 1} ${l}`));
 }
