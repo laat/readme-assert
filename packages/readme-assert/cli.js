36a4218981b3298fe74e746fd640f692ba0c95f8
Sigurd Fosseng <sigurd@fosseng.net>
2019-06-11 18:31:29 +0200

    node 8

--- packages/readme-assert/cli.js
+++ packages/readme-assert/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable */
-if (process.version.match(/v(\d+)\./)[1] <= 8) {
+if (process.version.match(/v(\d+)\./)[1] < 8) {
   console.error(
     "readme-assert: Node 8 or greater is required. `readme-assert` did not run."
   );
